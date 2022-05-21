321915f5167b3911604b9db7d5c8bca495ed815c
Rane <ankit.rane@here.com>
2022-05-21 21:16:55 +0530

    new portfolio started. responsive, navbar, popup pending

--- JavaScript Interview/Resume/script.js
+++ JavaScript Interview/Resume/script.js
@@ -1,5 +1,4 @@
 "use strict";
-
 //Change Link Style
 const navLink = document.querySelectorAll(".nav-link");
 const navLinks = document.querySelector(".nav-links");
@@ -13,7 +12,6 @@ navLinks.addEventListener("click", function (e) {
     clicked.classList.add("active");
   }
 });
-//Smooth Scrolling
 
 //Tabbed Component
 const btns = document.querySelectorAll(".btn__tab");
@@ -35,3 +33,55 @@ btnsTab.addEventListener("click", function (e) {
       .classList.add("education-content--active");
   }
 });
+
+//Skills Animation
+(function () {
+  var SkillsBar = function (bars) {
+    this.bars = document.querySelectorAll(bars);
+    if (this.bars.length > 0) {
+      this.init();
+    }
+  };
+
+  SkillsBar.prototype = {
+    init: function () {
+      var self = this;
+      self.index = -1;
+      self.timer = setTimeout(function () {
+        self.action();
+      }, 500);
+    },
+    select: function (n) {
+      var self = this,
+        bar = self.bars[n];
+
+      if (bar) {
+        var width = bar.parentNode.dataset.percent;
+
+        bar.style.width = width;
+        bar.parentNode.classList.add("complete");
+      }
+    },
+    action: function () {
+      var self = this;
+      self.index++;
+      if (self.index == self.bars.length) {
+        clearTimeout(self.timer);
+      } else {
+        self.select(self.index);
+      }
+
+      setTimeout(function () {
+        self.action();
+      }, 500);
+    },
+  };
+
+  window.SkillsBar = SkillsBar;
+})();
+
+(function () {
+  document.addEventListener("DOMContentLoaded", function () {
+    var skills = new SkillsBar(".skillbar-bar");
+  });
+})();
