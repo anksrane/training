9b9229fd02e7bf9d98cbc3fa10ff5da990862963
anksrane <anks.rane@gmail.com>
2022-06-15 11:29:06 +0530

    restructured folder

--- JavaScript Course Files/Practice/05_04/script.js
+++
@@ -1,6 +0,0 @@
-/**
- * Note: This file is intentionally empty.
- * You can use it to test your skills at traversing the DOM using JavaScript.
- */
-const ulList=document.querySelectorAll(".backpack__features");
-console.log(ulList.length)
--- Programming/varibaleSwap.js
+++
@@ -1,5 +0,0 @@
-var a=prompt('Eneter first Variable:');
-var b=prompt('Eneter Second Variable:');
-console.log("Before Swap First varible is",a," Second variable is ",b);
-[a,b]=[b,a];
-console.log("After Swap First varible is",a," Second variable is ",b);
